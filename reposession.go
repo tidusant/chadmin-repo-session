37a4738ffa893650a5f1c7855e09aa48fc331f64
duy ha <duyhph@gmail.com>
2020-07-05 16:36:04 +0300

    create

--- reposession.go
+++ reposession.go
@@ -31,7 +31,7 @@ func init() {
 func CreateSession() string {
 	sex := mystring.RandString(20)
 	col := db.C("sessions")
-	err := col.Insert(bson.M{"sex": sex, "created": time.Now().Unix(), "expired": time.Now().Unix() + 30*60})
+	err := col.Insert(bson.M{"uid": sex, "created": time.Now().Unix(), "expired": time.Now().Unix() + 30*60})
 	if c3mcommon.CheckError("Insert sessions", err) {
 		return sex
 	}
@@ -43,10 +43,10 @@ func CheckSession(s string) bool {
 	}
 	col := db.C("sessions")
 	var result models.Session
-	err2 := col.Find(bson.M{"sex": s}).One(&result)
+	err2 := col.Find(bson.M{"uid": s}).One(&result)
 
 	if err2 != nil {
-		log.Infof("Session not found sex '%s': %s\n", s, err2)
+		log.Infof("Session not found uid '%s': %s\n", s, err2)
 	} else {
 		if result.Expired > time.Now().Unix() {
 			//update session
@@ -57,26 +57,29 @@ func CheckSession(s string) bool {
 		} else {
 			//remove session
 			col.RemoveId(result.ID)
-			log.Infof("Session expired: sex '%s'", s)
+			log.Infof("Session expired: uid '%s'", s)
 			return false
 		}
+
 	}
+
 	return false
 }
 func CheckRequest(uri, useragent, referrer, remoteAddress, requestType string) bool {
 
 	col := db.C("requestUrls")
 
-	urlcount, _ := col.Find(bson.M{"uri": uri, "type": requestType, "created": bson.M{"$gt": int(time.Now().Unix()) - 1*3600}}).Count()
-	if urlcount <= viper.GetInt("config.sameurllimit") {
+	urlcount, _ := col.Find(bson.M{"uri": uri, "created": bson.M{"$gt": int(time.Now().Unix()) - 1}}).Count()
+	if urlcount < 50 {
+		log.Debugf("same url count %d", urlcount)
 		//check ip in 3 sec
 		urlcount, err := col.Find(bson.M{"remoteAddress": remoteAddress, "created": bson.M{"$gt": int(time.Now().Unix()) - 3}}).Count()
-		if urlcount < viper.GetInt("config.requestlimit") {
-			err = col.Insert(bson.M{"uri": uri, "created": int(time.Now().Unix()), "user-agent": useragent, "referer": referrer, "remoteAddress": remoteAddress, "type": requestType})
+		if urlcount < 500 {
+			err = col.Insert(bson.M{"uri": uri, "created": int(time.Now().Unix()), "user-agent": useragent, "referer": referrer, "remoteAddress": remoteAddress})
 			c3mcommon.CheckError("checkRequest Insert", err)
 			return true
 		} else {
-			log.Debugf("request ip limited %s", uri)
+			log.Debugf("old request ip limited %s", uri)
 		}
 	} else {
 		log.Debugf("request limited %s", uri)
